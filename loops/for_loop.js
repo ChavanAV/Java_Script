4802fd9990df22c45785d716f6ca718f1cacf30f
Akshay Chavan <akshaychavan5080@gmail.com>
2024-09-16 13:17:17 +0530

    16-9-24 find vowels using for loop from string

---
+++ loops/for_loop.js
@@ -0,0 +1,13 @@
+
+
+let str = "Hello hunny bunny"
+
+let findVowels = (val) => {
+    for (let val = 0; val < array.length; val++) {
+        if (val[i] == 'a', val[i] == 'e', val[i] == 'i', val[i] == 'o', val[i] == 'u') {
+            console.log(val[i]);
+        }
+    }
+}
+
+findVowels(str);
