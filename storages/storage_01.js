3afb73a42956e2021cb11ae6394e3b47cbc5bdb3
Akshay Chavan <akshaychavan5080@gmail.com>
2024-10-23 13:07:44 +0530

    21-10-24 session and local storage

---
+++ storages/storage_01.js
@@ -0,0 +1,12 @@
+// let password = '1234578';
+// let userName = 'asdgadahdb';
+
+// sessionStorage.setItem('pass', password);
+// sessionStorage.setItem('un', userName);
+
+// console.log(sessionStorage.getItem('un'));
+
+// localStorage.setItem('pass', password)
+// localStorage.setItem('un', userName)
+
+// localStorage.clear()
